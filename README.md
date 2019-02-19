cc70797cea8f2f5371cbcfc935ff4a1ad42986e6
starf <starf@Joseph>
2019-02-19 13:46:49 -0700

    Setting up logging class in data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CSE326Project
-# Hello World
+
+
+Have tests stored as "test1.csv" containing sequence a on line 1, sequence b on line 2, and the correct answer on line 3.
+
+
---
+++ src/ASMR/Data/Test_Logger.java
@@ -0,0 +1,28 @@
+package ASMR.Data;
+
+import java.util.HashMap;
+
+/**
+ * Class for logging tests
+ * Before presenting tests, initiate a single object of this class
+ * Class methods will be called by the gui action handlers
+ * Links test number and subject response
+ * 
+ * @author Joseph Ladino
+ *
+ */
+
+public class Test_Logger {
+
+	private HashMap<Integer, String> results;
+	public Test_Logger() {
+		results = new HashMap<Integer, String>();
+	}
+	public void Log_Yes() {
+		
+	}
+	public void Log_No() {
+		
+	}
+	
+}
