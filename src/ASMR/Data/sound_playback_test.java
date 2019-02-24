54db911d3c45b8210fdc650ea529f150b083afc8
starf <starf@Joseph>
2019-02-24 13:35:21 -0700

    Some more work on response logging and a test for .wav playback

---
+++ src/ASMR/Data/sound_playback_test.java
@@ -0,0 +1,43 @@
+package ASMR.Data;
+
+import java.io.File;
+
+import javax.sound.sampled.*;
+
+public class sound_playback_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			File testfile = new File("killedkenny.wav");
+			AudioInputStream st;
+			AudioFormat fmt;
+			DataLine.Info info;
+			Clip clip;
+		
+			st = AudioSystem.getAudioInputStream(testfile);
+			fmt = st.getFormat();
+			info = new DataLine.Info(Clip.class, fmt);
+			clip = (Clip) AudioSystem.getLine(info);
+			
+			clip.addLineListener(new LineListener() {
+				@Override
+				public void update(LineEvent event) {
+					if(event.getType() == LineEvent.Type.STOP) {
+						clip.close();
+					}
+				}
+			});
+			
+			clip.open(st);
+			clip.start();
+			Thread.sleep(4000);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
