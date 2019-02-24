48895035836794967451e964d48ca59067dc431a
starf <starf@Joseph>
2019-02-24 13:35:21 -0700

    Some more work on response logging and a test for .wav playback

---
+++ src/ASMR/Data/Test.java
@@ -0,0 +1,37 @@
+package ASMR.Data;
+/**
+ * This class will hold two objects of Sequence which are test sequences
+ * A and B. It will also hold the String of "yes" or "no" for response comparison.
+ * @author starf
+ *
+ */
+
+import java.io.*;
+import javax.sound.sampled.*;
+
+public class Test {
+	
+	private String id;
+	private String answer;
+	//private Sequence first;
+	//private Sequence second;
+	
+	public String getId() {
+		return this.id;
+	}
+	public String getAnswer() {
+		return this.answer;
+	}
+	public void setID() {
+		//TBD
+	}
+	public void setAnswer() {
+		//TBD
+	}
+	
+	/*
+	public void PlaySequence(Sequence sec) {
+		
+	}
+	*/
+}
--- src/ASMR/Data/Test_Logger.java
+++ src/ASMR/Data/Test_Logger.java
@@ -14,15 +14,49 @@ import java.util.HashMap;
 
 public class Test_Logger {
 
-	private HashMap<Integer, String> results;
+	private HashMap <String, Boolean> results = null;
+	/**
+	 * constructor to initiate the logger before tests are run
+	 */
 	public Test_Logger() {
-		results = new HashMap<Integer, String>();
+		results = new HashMap<String, Boolean>();
 	}
-	public void Log_Yes(int test) {
+	/**
+	 * record subject yes response
+	 * @param test: object with test data
+	 */
+	public void Log_Yes(Test test) {
+		String tmp = test.getAnswer();
+		if(tmp.toLowerCase().compareTo("yes") == 0) {
+			//boolean true is a correct answer
+			results.put(test.getId(), true);
+		} else {
+			//boolean false is an incorrect answer
+			results.put(test.getId(), false);
+		}
 		
 	}
-	public void Log_No(int test) {
-		
+	/**
+	 * record subject no response
+	 * @param test: object with test data
+	 */
+	public void Log_No(Test test) {
+		String tmp = test.getAnswer();
+		if(tmp.toLowerCase().compareTo("no") == 0) {
+			//boolean true is a correct answer
+			results.put(test.getId(), true);
+		} else {
+			//boolean false is an incorrect answer
+			results.put(test.getId(), false);
+		}
+	}
+	
+	public void SaveResults() {
+		/*
+		 * Get intended fpath
+		 * create new csv file
+		 * populate via hashmap.getkeys and getting values
+		 */
 	}
 	
 }
