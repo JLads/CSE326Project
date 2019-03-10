fb1683b08928112ad696508edc3ecc06668a614c
Joseph <starfleetjl1@gmail.com>
2019-03-10 15:28:00 -0600

    Reworked the GUI interface for test retrieval.
    Added a directory for the test data csv files and one for the test logs.

---
+++ src/ASMR/Data/TestList.java
@@ -0,0 +1,70 @@
+package ASMR.Data;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ * This class implements the functions of the retriever interface.
+ * It also serves as a location to store the actual list of test objects
+ * @author Joseph
+ *
+ */
+public class TestList implements TestRetriever {
+
+	//The list of test objects
+	private ArrayList<Test> tests;
+	
+	/**
+	 * Initializes the object list on instantiation
+	 */
+	public TestList() {
+		tests = new ArrayList<Test>();
+	}
+	
+	@Override
+	public int BuildList(String fp) {
+		//Put calls to utility parser here
+		
+		//MyPasing for debug purpose
+		try {
+			File tmp = new File(fp);
+			Scanner sc = new Scanner(tmp);
+			while(sc.hasNextLine()) {
+				tests.add(new Test(sc.nextLine(), sc.nextLine(), sc.nextLine()));
+				if(sc.hasNextLine()) {
+					sc.nextLine();
+				}
+			}
+			sc.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return tests.size();
+	}
+
+	@Override
+	public ArrayList<String> getSequence(int testnum, int seqID) {
+		// TODO Auto-generated method stub
+		switch(seqID) {
+		case 1:
+			//return tests.get(testnum).getFirst();
+			return tests.get(testnum).listFirst();
+		case 2:
+			//return tests.get(testnum).getSecond();
+			return tests.get(testnum).listSecond();
+		default:
+			//return tests.get(testnum).getFirst();
+			return tests.get(testnum).listFirst();
+		}
+	}
+
+	@Override
+	public String getAnswer(int testnum) {
+		// TODO Auto-generated method stub
+		return tests.get(testnum).getAnswer();
+	}
+
+}
---
+++ src/ASMR/Data/TestRetriever.java
@@ -0,0 +1,37 @@
+package ASMR.Data;
+
+import java.util.ArrayList;
+
+
+/**
+ * This is the test retrieval interface for the GUI.
+ * The gui can call "TestRetriever tmp = new TestList();" to gain
+ * access to the test objects and stimuli sequences.
+ * @author Joseph
+ *
+ */
+public interface TestRetriever {
+
+	/**
+	 * Builds the list of test objects and reports the number of tests
+	 * in queue
+	 * @param fp path to the data source csv file
+	 * @return the number of test objects in the list
+	 */
+	public abstract int BuildList(String fp);
+	
+	/**
+	 * returns a specific sequence from a specific test
+	 * @param testnum index of desired test object
+	 * @param i takes 1 or 2: the sequence to return
+	 * @return array list of file paths for each audio file in a sequence
+	 */
+	public abstract ArrayList<String> getSequence(int testnum, int i);
+	
+	/**
+	 * gets the correct response string from a specified test object
+	 * @param testnum index of desired test object
+	 * @return correct response as a String
+	 */
+	public abstract String getAnswer(int testnum);
+}
