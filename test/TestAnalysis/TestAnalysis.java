92b25166ceebec87f1c31351ccb4bbaac250cd2a
Steven Anaya <steven.anaya@cs.nmt.edu>
2019-04-16 03:24:58 -0600

    Test class for Analysis methods

---
+++ test/TestAnalysis/TestAnalysis.java
@@ -0,0 +1,16 @@
+package TestAnalysis;
+
+import ASMR.Analysis.RAnalysis;
+
+/**
+ * Test file for R data analysis methods.
+ * @author Steven Anaya
+ *
+ */
+public class TestAnalysis {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ test/TestAnalysis/test.r
@@ -0,0 +1,3 @@
+getwd()
+ts(1:20)
+for(i in 1:3) print(1:i)
