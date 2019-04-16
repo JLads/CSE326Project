5d5f1a39317825c3f65732f90e2b8222ebf8dd77
Niko25851 <niko25851@gmail.com>
2019-04-16 00:30:46 -0600

    finished wave file selection fields

--- src/ASMR/GUI/TestGenerationPanel.java
+++ src/ASMR/GUI/TestGenerationPanel.java
@@ -18,29 +18,45 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import ASMR.Data.CreateTestFiles;
 
+/**
+ * a panel for building test files
+ * @author nicholas
+ * TODO fix file and directory path processing so the dir is subtracted from file.path instead of using file.name
+ */
 public class TestGenerationPanel extends AbstractPanel{
 
 	private static final long serialVersionUID = -1308533711316270123L;
 
-	private String directory;
-	private ArrayList<String> seq1;
-	private ArrayList<String> seq2;
-	private Boolean answer;
+	private String directory;		//path to directory
+	private ArrayList<String> seq1;	//array list of file names for sequence 1
+	private ArrayList<String> seq2; //array list of file names for sequence 2
+	private Boolean answer;			//correct answer for a test
 	
-	Font labelFont = new Font("Aerial", Font.PLAIN, 15);
-	Font buttonFont = new Font("Aerial", Font.PLAIN, 20);
-	
+	//fonts for labels and buttons
+	private Font labelFont = new Font("Aerial", Font.PLAIN, 20);
+	private Font buttonFont = new Font("Aerial", Font.PLAIN, 25);
 
-	FileNameExtensionFilter wavFilter = new FileNameExtensionFilter("Wav FILE", "wav");
+	//file filter for wav file choosers
+	private FileNameExtensionFilter wavFilter = new FileNameExtensionFilter("WAV FILE", "wav");
 	
-	ArrayList<JFileChooser> fileChoosers = new ArrayList<JFileChooser>();
+	//array list of file choosers, for updating with new directory paths
+	private ArrayList<JFileChooser> fileChoosers = new ArrayList<JFileChooser>();
 	
+	//array list of file choosers, for updating with new directory paths
+	private ArrayList<JTextArea> fileTextAreas = new ArrayList<JTextArea>();
+	
+	/**
+	 * default constructor
+	 */
 	public TestGenerationPanel() {
 		this.initialize();
 		CreateTestFiles.proxyClear();
 		this.buildPanel();
 	}
 	
+	/**
+	 * initializes the variables to their base values
+	 */
 	public void initialize() {
 		this.directory = System.getProperty("user.home");
 		this.seq1 = new ArrayList<String>(5);
@@ -60,6 +76,9 @@ public class TestGenerationPanel extends AbstractPanel{
 		gbc.insets = new Insets(10, 10, 10, 10);
 		gbc.fill = GridBagConstraints.BOTH;
 		
+		JLabel label = new JLabel("Directory:");
+		label.setFont(labelFont);
+		
 		JFileChooser chooser = new JFileChooser();
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		
@@ -78,6 +97,14 @@ public class TestGenerationPanel extends AbstractPanel{
 					directory = file.getPath();
 					for(JFileChooser jfc : fileChoosers) {
 						jfc.setCurrentDirectory(new File(directory));
+						jfc.setSelectedFile(null);
+					}
+					for(JTextArea jta : fileTextAreas) {
+						jta.setText(null);
+					}
+					for(int i = 0; i < 5; i++) {
+						seq1.set(i, null);
+						seq2.set(i, null);
 					}
 					text.setText(directory);
 				}
@@ -86,21 +113,25 @@ public class TestGenerationPanel extends AbstractPanel{
 		
 		gbc.gridy=0;
 		gbc.gridx=0;
-		gbc.ipadx=100;
+		gbc.ipadx=1200;
+		dirSelectorPanel.add(label, gbc);
+		gbc.ipadx=0;
+		
+		gbc.gridy=1;
+		gbc.gridx=0;
 		dirSelectorPanel.add(text, gbc);
 		
-		gbc.gridy=0;
-		gbc.gridx=1;
-		gbc.ipadx=0;
+		gbc.gridy=1;
+		gbc.gridx=2;
 		dirSelectorPanel.add(browse, gbc);
 		
 		return dirSelectorPanel;
 	}
 	
 	/**
-	 * builds a sub panel for selecting files in a directory
+	 * builds a sub panel for selecting wav files in a directory
 	 * @param seq the array list for a sequence
-	 * @return a jpannel for selecting sequences
+	 * @return a jpanel for selecting sequences
 	 */
 	private JPanel buildSequenceSelector(ArrayList<String> seq) {
 		JPanel panel = new JPanel();
@@ -114,16 +145,19 @@ public class TestGenerationPanel extends AbstractPanel{
 		file1Label.setFont(this.labelFont);
 		gbc.gridy=0;
 		gbc.gridx=0;
+		gbc.ipadx=500;
 		panel.add(file1Label, gbc);
+		gbc.ipadx=0;
 		
 		JFileChooser f1chooser = new JFileChooser();
 		f1chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-		f1chooser.setFileFilter(wavFilter);
-		fileChoosers.add(f1chooser);
+		f1chooser.setFileFilter(this.wavFilter);
+		this.fileChoosers.add(f1chooser);
 		
 		JTextArea f1text = new JTextArea();
 		f1text.setFont(this.labelFont);
 		f1text.setEditable(false);
+		this.fileTextAreas.add(f1text);
 		
 		JButton f1browse = new JButton("Browse");
 		f1browse.setFont(this.buttonFont);
@@ -133,7 +167,7 @@ public class TestGenerationPanel extends AbstractPanel{
 				int retVal = f1chooser.showOpenDialog(TestGenerationPanel.this);
 				if(retVal == JFileChooser.APPROVE_OPTION) {
 					File file = f1chooser.getSelectedFile();
-					seq.add(0, file.getPath());
+					seq.add(0, file.getName());
 					f1text.setText(seq.get(0));
 				}
 			}
@@ -141,11 +175,9 @@ public class TestGenerationPanel extends AbstractPanel{
 		
 		gbc.gridy=1;
 		gbc.gridx=0;
-		gbc.ipadx=250;
 		panel.add(f1text, gbc);
 		gbc.gridy=1;
 		gbc.gridx=1;
-		gbc.ipadx=0;
 		panel.add(f1browse, gbc);
 		
 		JLabel file2Label = new JLabel("File 2:");
@@ -154,7 +186,36 @@ public class TestGenerationPanel extends AbstractPanel{
 		gbc.gridx=0;
 		panel.add(file2Label, gbc);
 		
+		JFileChooser f2chooser = new JFileChooser();
+		f2chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		f2chooser.setFileFilter(this.wavFilter);
+		this.fileChoosers.add(f2chooser);
 		
+		JTextArea f2text = new JTextArea();
+		f2text.setFont(this.labelFont);
+		f2text.setEditable(false);
+		this.fileTextAreas.add(f2text);
+		
+		JButton f2browse = new JButton("Browse");
+		f2browse.setFont(this.buttonFont);
+		f2browse.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int retVal = f2chooser.showOpenDialog(TestGenerationPanel.this);
+				if(retVal == JFileChooser.APPROVE_OPTION) {
+					File file = f2chooser.getSelectedFile();
+					seq.add(1, file.getName());
+					f2text.setText(seq.get(1));
+				}
+			}
+		});
+		
+		gbc.gridy=3;
+		gbc.gridx=0;
+		panel.add(f2text, gbc);
+		gbc.gridy=3;
+		gbc.gridx=1;
+		panel.add(f2browse, gbc);
 		
 		JLabel file3Label = new JLabel("File 3:");
 		file3Label.setFont(this.labelFont);
@@ -162,7 +223,36 @@ public class TestGenerationPanel extends AbstractPanel{
 		gbc.gridx=0;
 		panel.add(file3Label, gbc);
 		
+		JFileChooser f3chooser = new JFileChooser();
+		f3chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		f3chooser.setFileFilter(this.wavFilter);
+		this.fileChoosers.add(f3chooser);
 		
+		JTextArea f3text = new JTextArea();
+		f3text.setFont(this.labelFont);
+		f3text.setEditable(false);
+		this.fileTextAreas.add(f3text);
+		
+		JButton f3browse = new JButton("Browse");
+		f3browse.setFont(this.buttonFont);
+		f3browse.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int retVal = f3chooser.showOpenDialog(TestGenerationPanel.this);
+				if(retVal == JFileChooser.APPROVE_OPTION) {
+					File file = f3chooser.getSelectedFile();
+					seq.add(2, file.getName());
+					f3text.setText(seq.get(2));
+				}
+			}
+		});
+		
+		gbc.gridy=5;
+		gbc.gridx=0;
+		panel.add(f3text, gbc);
+		gbc.gridy=5;
+		gbc.gridx=1;
+		panel.add(f3browse, gbc);
 		
 		JLabel file4Label = new JLabel("File 4:");
 		file4Label.setFont(this.labelFont);
@@ -170,7 +260,36 @@ public class TestGenerationPanel extends AbstractPanel{
 		gbc.gridx=0;
 		panel.add(file4Label, gbc);
 		
+		JFileChooser f4chooser = new JFileChooser();
+		f4chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		f4chooser.setFileFilter(this.wavFilter);
+		this.fileChoosers.add(f4chooser);
 		
+		JTextArea f4text = new JTextArea();
+		f4text.setFont(this.labelFont);
+		f4text.setEditable(false);
+		this.fileTextAreas.add(f4text);
+		
+		JButton f4browse = new JButton("Browse");
+		f4browse.setFont(this.buttonFont);
+		f4browse.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int retVal = f4chooser.showOpenDialog(TestGenerationPanel.this);
+				if(retVal == JFileChooser.APPROVE_OPTION) {
+					File file = f4chooser.getSelectedFile();
+					seq.add(3, file.getName());
+					f4text.setText(seq.get(3));
+				}
+			}
+		});
+		
+		gbc.gridy=7;
+		gbc.gridx=0;
+		panel.add(f4text, gbc);
+		gbc.gridy=7;
+		gbc.gridx=1;
+		panel.add(f4browse, gbc);
 		
 		JLabel file5Label = new JLabel("File 5:");
 		file5Label.setFont(this.labelFont);
@@ -178,9 +297,73 @@ public class TestGenerationPanel extends AbstractPanel{
 		gbc.gridx=0;
 		panel.add(file5Label, gbc);
 		
+		JFileChooser f5chooser = new JFileChooser();
+		f5chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		f5chooser.setFileFilter(this.wavFilter);
+		this.fileChoosers.add(f5chooser);
 		
+		JTextArea f5text = new JTextArea();
+		f5text.setFont(this.labelFont);
+		f5text.setEditable(false);
+		this.fileTextAreas.add(f5text);
+		
+		JButton f5browse = new JButton("Browse");
+		f5browse.setFont(this.buttonFont);
+		f5browse.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int retVal = f5chooser.showOpenDialog(TestGenerationPanel.this);
+				if(retVal == JFileChooser.APPROVE_OPTION) {
+					File file = f5chooser.getSelectedFile();
+					seq.add(4, file.getName());
+					f5text.setText(seq.get(4));
+				}
+			}
+		});
+		
+		gbc.gridy=9;
+		gbc.gridx=0;
+		panel.add(f5text, gbc);
+		gbc.gridy=9;
+		gbc.gridx=1;
+		panel.add(f5browse, gbc);
 		
 		return panel;	
+	}
+	
+	/**
+	 * builds a sub panel for sequence selection
+	 * @return sub panel for sequence selection
+	 */
+	private JPanel sequenceSelectors() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(10, 10, 10, 10);
+		gbc.fill = GridBagConstraints.BOTH;
+		
+		//Sequence 1 selector
+		JLabel seq1Label = new JLabel("Sequence 1");
+		seq1Label.setFont(labelFont);
+		gbc.gridy=0;
+		gbc.gridx=0;
+		panel.add(seq1Label, gbc);
+				
+		gbc.gridy=1;
+		gbc.gridx=0;
+		panel.add(this.buildSequenceSelector(this.seq1), gbc);
+				
+		//sequence 2 selector
+		JLabel seq2Label = new JLabel("Sequence 2");
+		seq2Label.setFont(labelFont);
+		gbc.gridy=0;
+		gbc.gridx=1;
+		panel.add(seq2Label, gbc);
+				
+		gbc.gridy=1;
+		gbc.gridx=1;
+		panel.add(this.buildSequenceSelector(this.seq2), gbc);
+		return panel;
 	}
 	
 	@Override
@@ -190,39 +373,13 @@ public class TestGenerationPanel extends AbstractPanel{
 		gbc.insets = new Insets(10, 10, 10, 10);
 		gbc.fill = GridBagConstraints.BOTH;
 		
-		//directory selector
-		JLabel dirLabel = new JLabel("Directory:");
-		dirLabel.setFont(labelFont);
 		gbc.gridy=0;
-		gbc.gridx=0;
-		this.add(dirLabel, gbc);
-		
-		gbc.gridy=1;
 		gbc.gridx=0;
 		this.add(this.buildDirectorySelector(), gbc);
 		
-		//Sequence 1 selector
-		JLabel seq1Label = new JLabel("Sequence 1");
-		seq1Label.setFont(labelFont);
-		gbc.gridy=2;
+		gbc.gridy=1;
 		gbc.gridx=0;
-		this.add(seq1Label, gbc);
-		
-		gbc.gridy=3;
-		gbc.gridx=0;
-		this.add(this.buildSequenceSelector(this.seq1), gbc);
-		
-		//sequence 2 selector
-		JLabel seq2Label = new JLabel("Sequence 2");
-		seq2Label.setFont(labelFont);
-		gbc.gridy=2;
-		gbc.gridx=1;
-		this.add(seq2Label, gbc);
-		
-		gbc.gridy=3;
-		gbc.gridx=1;
-		this.add(this.buildSequenceSelector(this.seq2), gbc);
-		
+		this.add(this.sequenceSelectors(), gbc);
 	}
 
 }
