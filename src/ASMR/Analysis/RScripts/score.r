d9005db5bdda89344ba61be3a5210630d6a6409b
Steven Anaya <steven.anaya@cs.nmt.edu>
2019-04-21 22:32:41 -0600

    Script uses results path to save plot

--- src/ASMR/Analysis/RScripts/score.r
+++ src/ASMR/Analysis/RScripts/score.r
@@ -1,11 +1,13 @@
-data.file = commandArgs(trailingOnly = T)
+args = commandArgs(trailingOnly = T)
+data.file = args[1]
+results.file = args[2]
 data.file.name = sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(data.file))
 
 test.file = readLines(data.file, n = 1)
 test.results = read.csv(data.file, skip = 1)
 
 plot.file.name = paste(data.file.name, "score.png", sep = "_")
-plot.path = paste(dirname(data.file), plot.file.name, sep="/")
+plot.path = paste(dirname(results.file), plot.file.name, sep="/")
 plot.title = paste(data.file.name, "Score over Tests")
 
 png(plot.path)
