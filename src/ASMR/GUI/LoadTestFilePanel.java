e1452cb54479233315a9b925daa2c490112275e5
Niko25851 <niko25851@gmail.com>
2019-04-22 18:31:52 -0600

    added a return button to LoadTesFilePanel

--- src/ASMR/GUI/LoadTestFilePanel.java
+++ src/ASMR/GUI/LoadTestFilePanel.java
@@ -1,5 +1,12 @@
 package ASMR.GUI;
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 import ASMR.Data.TestRetriever;
 
 /**
@@ -17,6 +24,19 @@ public class LoadTestFilePanel extends AbstractFileIOPanel{
 	public LoadTestFilePanel() {
 		this.panelLabel = "Enter the file path for a Test file to load.";
 		this.buildPanel();
+		
+		JButton returnButton = new JButton("Return");
+		returnButton.setFont(new Font("Aerial", Font.PLAIN, 20));
+		returnButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					AbstractPanel.getFrame().changePanel(new MainPanel());
+			}
+		});
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridy=3;
+		gbc.ipadx=501;
+		this.add(returnButton, gbc);
 	}
 
 	@Override
